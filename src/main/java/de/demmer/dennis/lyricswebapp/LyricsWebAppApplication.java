69b3731decc7d821e4917a4c99db54cd8a8c544d
DDemmer1 <Dennis.Demmer@web.de>
2018-05-05 13:45:25 +0200

    Initial commit

---
+++ src/main/java/de/demmer/dennis/lyricswebapp/LyricsWebAppApplication.java
@@ -0,0 +1,14 @@
+package de.demmer.dennis.lyricswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricsWebAppApplication.class, args);
+	}
+}
+
+
